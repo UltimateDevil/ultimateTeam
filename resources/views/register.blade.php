604a36b47db5e03a9fda47a066b35033ce8d4aaf
UltimateDevil <30384343+UltimateDevil@users.noreply.github.com>
2018-03-04 08:06:19 +0530

    working on css

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -19,11 +19,34 @@
 <body>
 
 <div class="fh5co-cover" role="banner" style="background-image:'http://localhost/ultimateTeam/public/resources/assets/images/img_bg_1.jpg';">
-    <div class="overlay"></div>
-    <div class="loginContainer">
+    <div class="overlay">
+        <div class="loginContainer">
+            <div class="loginBox">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Name :</label>
+                        <input type="text" class="form-control" id="name">
+                    </div>
 
+                    <div class="form-group">
+                        <label for="email">Email address:</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" class="form-control" id="pwd">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="cpwd">Confirm Password:</label>
+                        <input type="password" class="form-control" id="cpwd">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     </div>
-
 </div>
 
 </body>
